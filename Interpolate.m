05dac7eb8817a2e13547e1d5b2f16b82411eb493
shiokoala <ksksnd@gmail.com>
2020-11-17 12:30:58 +0900

    rao simulator

--- Interpolate.m
+++ Interpolate.m
@@ -10,6 +10,13 @@ Mx = T{:,19};
 My = T{:,22};
 Mz = T{:,25};
 
+Fx_deg = T{:,11};
+Fy_deg = T{:,14};
+Fz_deg = T{:,17};
+Mx_deg = T{:,20};
+My_deg = T{:,23};
+Mz_deg = T{:,26};
+
 
 P=cat(2,Fn,omega);
 
@@ -24,6 +31,12 @@ FMx=scatteredInterpolant(P,Mx);
 FMy=scatteredInterpolant(P,My);
 FMz=scatteredInterpolant(P,Mz);
 
+FFxd=scatteredInterpolant(P,Fx_deg);
+FFyd=scatteredInterpolant(P,Fy_deg);
+FFzd=scatteredInterpolant(P,Fz_deg);
+FMxd=scatteredInterpolant(P,Mx_deg);
+FMyd=scatteredInterpolant(P,My_deg);
+FMzd=scatteredInterpolant(P,Mz_deg);
 
 VFxq=FFx(Xq,Yq);
 VFyq=FFy(Xq,Yq);
@@ -32,5 +45,16 @@ VMxq=FMx(Xq,Yq);
 VMyq=FMy(Xq,Yq);
 VMzq=FMz(Xq,Yq);
 
-mesh(Xq,Yq,VMzq);
+VFxd=FFxd(Xq,Yq);
+VFyd=FFyd(Xq,Yq);
+VFzd=FFzd(Xq,Yq);
+VMxd=FMxd(Xq,Yq);
+VMyd=FMyd(Xq,Yq);
+VMzd=FMzd(Xq,Yq);
+
+mesh(Xq,Yq,VMyd);
+
+% writematrix(VFxd,'Fxd.csv') 
+% writematrix(VFzd,'Fzd.csv') 
+% writematrix(VMyd,'Myd.csv') 
 
