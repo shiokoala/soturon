b6efe476ad63a62ca2c5780166a43468ada43039
shiokoala <ksksnd@gmail.com>
2020-11-20 15:58:08 +0900

    separate into modules

--- ship.py
+++ ship.py
@@ -1,40 +1,35 @@
 import math
-import random
+import simulation_settings as ss
+import constants
 
-gg = 9.8
-rho = 997
-
- t = 0.0
- dt = 0.01
- k = 5
-beta = 0.25
+gg = constants.gg
+rho = constants.rho
+beta = constants.beta
+dt = constants.dt
 
 
-width = 1000
-height = 800
-
-
- 
- class Particle{
-   PVector pos
-   PVector vel
-   PVector acc
+class Particle:
+    posx = 0
+    posz = 0
+    velx = 0
+    velz = 0
+    accx = 0
+    accz = 0
+    
     angle = 0
     anglevel = 0
     angleacc = 0
-   
-    rho = 997
-    gg = 9.8
-   
-    beta = 0.25
-   
+
     pl = 3.11
-    ph = 0.70
+    ph = 0.34
     pw = 0.94
     pv = 0.0878
     mass = pv*100
-   
-    A11 = 0.3 * rho * pv//
+    cgh = 0.340
+
+    draught = 0
+
+    A11 = 0.3 * rho * pv
     A13 = 0
     A31 = 0
     A53 = 0.1 * rho * pv * pl
@@ -42,133 +37,73 @@ height = 800
     A33 = 0.3 * rho * pv
     A51 = 0.01* rho * pv * pl
     A55 = 0.07* rho * pv * pl * pl
-    B11 = 2.0* rho * pv * sqrt(gg/pl)
-    B53 = 0.05* rho * pv * pl * sqrt(gg/pl)
-    B35 = 0.4* rho * pv * pl * sqrt(gg/pl)
-    B33 = 2.0* rho * pv * sqrt(gg/pl)
-    B55 = 0.4* rho * pv * pl * pl * sqrt(gg/pl)
+    B11 = 2.0* rho * pv * math.sqrt(gg/pl)
+    B53 = 0.05* rho * pv * pl * math.sqrt(gg/pl)
+    B35 = 0.4* rho * pv * pl * math.sqrt(gg/pl)
+    B33 = 2.0* rho * pv * math.sqrt(gg/pl)
+    B55 = 0.4* rho * pv * pl * pl * math.sqrt(gg/pl)
     C11 = 0
     C53 = 0
     C35 = 0
     C33 = 11014
-   
-   
-   
-   
-   Particle( x,  y){
-     pos = new PVector(x,y)
-     vel = new PVector(0,0)
-     acc = new PVector(0,0)
-   }
-   void calcAcc(){
- 
-     PVector totalForce = new PVector(0.,0.)
-      totalMoment = 0
-         
-     totalForce.y += mass*gg //Gravity
-     PVector cP = ww.particles[round(pos.x)]
-     
-      draught = min(max(pos.y-cP.y,0), ph)
-     text(draught,800,450)
-     
-      nx = cos(angle - PI/2)
-      ny = sin(angle - PI/2)
-     
-     if(draught>0){
-       h111 =0
-       h1111 = 0
-       h112 =0
-       h113 =0
-       for(Wave w:ww.waves){
-          ax = w.omega*w.omega*w.amp*exp(w.k*(height/2-cP.y))*cos(w.omega*t + w.k*pos.x)
-          ay = -w.omega*w.omega*w.amp*exp(w.k*(height/2-cP.y))*sin(w.omega*t + w.k*pos.x)
-          pd = rho*gg*w.amp*exp(w.k*(height/2-cP.y))*sin(w.omega*t + w.k*pos.x)
-         totalForce.x += -pw*pl*pd*nx + A11*ax + A13*ay
-         totalForce.y += -pw*pl*pd*ny + A31*ax + A33*ay
-         totalMoment += A51*ax + A53*ay
-         if(pw*pl*pd*ny>h111) h111 = pw*pl*pd*ny
-         if(A11*ax>h112) h112 = A11*ax
-         if(A13*ay>h113) h113 = A13*ay
-         if(exp(w.k*(height/2-cP.y))>h1111) h1111 = exp(w.k*(height/2-cP.y))
-       }
-        y_t = -(height/2 - pos.y)
-       h1 = (totalForce.y - B33*(vel.y + dt/2*acc.y) - C33*(y_t+dt*vel.y+(0.5-beta)*dt*dt*acc.y))
-       h11 = totalForce.y
-       h12 = - B33*(vel.y + dt/2*acc.y)
-       h13 = - C33*(y_t+dt*vel.y+(0.5-beta)*dt*dt*acc.y)
-       h2 = ((mass+A33) + dt/2*B33 + beta*dt*dt*C33)
-        heave = (totalForce.y - B33*(vel.y + dt/2*acc.y) - C33*(y_t+dt*vel.y+(0.5-beta)*dt*dt*acc.y)) / ((mass+A33) + dt/2*B33 + beta*dt*dt*C33)
-        surge = (totalForce.x - B11*(vel.x + dt/2*acc.x)) / ((mass+A11) + dt/2*B11)
-       
-       text(heave,800,650)
-       
-       acc.x = surge
-       acc.y = heave
-     }else{
-       acc.x = 0
-       acc.y = 9.8
-     }
-     
- 
-     pushMatrix()
-     translate(pos.x,pos.y)
-     line(0,0,acc.x,acc.y)
-     popMatrix()
-   }
-   
-   void update(){
-     calcAcc()
-     if(keyPressed==true){
-       if(keyCode == RIGHT){
-         acc.x+=1
-       }
-       if(keyCode == LEFT){
-         acc.x-=1
-       }
-     }
-     
-     vel = vel.add(acc.copy().mult(dt))
-     pos = pos.add(vel.copy().mult(dt))
-     pushMatrix()
-     translate(pos.x,pos.y)
-     line( pl/2, ph/2,-pl/2, ph/2)
-     line(-pl/2, ph/2,-pl/2,-ph/2)
-     line(-pl/2,-ph/2, pl/2,-ph/2)
-     line( pl/2,-ph/2, pl/2, ph/2)
-     popMatrix()
-   }
- }
+    C55 = 6233
 
+    def __init__(self,x,z):
+        self.posx = x
+        self.posz = z
+        self.accx = 0
+        self.accz = 0
+        self.velx = 0
+        self.velz = 0
 
-##############################################################################
+    def calcAcc(self,ww,t,thr_f):
+        tFx=0
+        tFz=0
+        tMy=0
 
-##############################################################################
+        average_waveheight = 0
+        res = 50
+        for i in range(res):
+          average_waveheight += ww.get(self.posx-self.pl/2+self.pl/res*i,t)
+        average_waveheight = average_waveheight/res
+        self.draught = min(max(average_waveheight-(self.posz-self.ph),0), self.ph)
+            
+        nx = math.cos(self.angle - math.pi/2)
+        ny = math.sin(self.angle - math.pi/2)
 
-class Wave:
-    amp = 0.
-    k = 0.
-    omega = 0.
-    phase = 0.
-    def __init__(self, a, o, p):
-        self.amp = a
-        self.omega = o
-        self.k = o*o/gg
-        self.phase = p
-    
-    def get(self,x, t):
-        return self.amp*math.sin(self.omega*t + self.k*x + self.phase)
+        Fn = math.fabs(self.velx)/math.sqrt(9.8*self.pl)
 
+        tFz -= self.mass*gg #Gravity
+        
+        if(self.draught>0):
+            tFx += thr_f * math.cos(self.angle)
+            tFz += thr_f * math.sin(self.angle)
 
+            for w in ww.waves:
+                omega_e = w.omega + w.omega**2 * self.velx / gg
+                tFx += w.calcF(self.posx,t,Fn,omega_e,axis=1)
+                tFz += w.calcF(self.posx,t,Fn,omega_e,axis=3)
+                tMy += w.calcF(self.posx,t,Fn,omega_e,axis=5)
 
-class Water:
-    waves = []
-    def __init__(self, num):
-        for i in range(num):
-            newWave = Wave(random.uniform(0.01,0.1),random.uniform(0.01,1),random.uniform(1,10))
-            self.waves.append(newWave)
-    def get(self,x,t):
-        waveheight = 0
-        for w in self.waves:
-            waveheight += w.get(x,t)
-        return waveheight
+            z_t = self.posz - 0.185
+            heave = (tFz - self.B33*(self.velz + dt/2*self.accz) - self.C33*(z_t+dt*self.velz+(0.5-beta)*dt*dt*self.accz)) / ((self.mass+self.A33) + dt/2*self.B33 + beta*dt*dt*self.C33)
+            surge = (tFx - self.B11*(self.velx + dt/2*self.accx)) / ((self.mass+self.A11) + dt/2*self.B11)
+            pitch = (tMy - self.B55*(self.anglevel + dt/2*self.angleacc) - self.C55*(self.angle+dt*self.anglevel+(0.5-beta)*dt*dt*self.angleacc)) / ((self.mass+self.A55) + dt/2*self.B55 + beta*dt*dt*self.C55)
 
+            self.accx = surge
+            self.accz = heave
+            self.angleacc = pitch
+        else:
+          self.accx = 0
+          self.accz = -9.8
+
+    def update(self,ww,t,thr_f):
+      self.calcAcc(ww,t,thr_f)
+      self.velx += self.accx*dt
+      self.velz += self.accz*dt
+      self.anglevel += self.angleacc*dt
+      self.posx += self.velx*dt
+      self.posz += self.velz*dt
+      self.angle += self.anglevel*dt
+      # print("x=" + "{:.2f}".format(self.posx))
+      # print("z=" + "{:.2f}".format(self.posz))
---
+++ simulation_settings.py
@@ -0,0 +1,107 @@
+import math
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import constants
+
+
+df1=pd.read_csv("Fx.csv",header=-1) #fnq= 41 oq=61  df1[fnq][oq]
+df3=pd.read_csv("Fz.csv",header=-1)
+df5=pd.read_csv("My.csv",header=-1)
+matrixList=[df1,df3,df5]
+ddf1=pd.read_csv("Fxd.csv",header=-1)
+ddf3=pd.read_csv("Fzd.csv",header=-1)
+ddf5=pd.read_csv("Myd.csv",header=-1)
+dmatrixList=[ddf1,ddf3,ddf5]
+
+
+gg = constants.gg
+rho = constants.rho
+beta = constants.beta
+dt = constants.dt
+
+class Wave:
+    amp = 0.
+    k = 0.
+    omega = 0.
+    phase = 0.
+    def __init__(self, a, o, p):
+        self.amp = a
+        self.omega = o
+        self.k = o*o/gg
+        self.phase = p
+    
+    def get(self,x, t):
+        return self.amp*math.sin(self.omega*t + self.k*x + self.phase)
+
+    def calcF(self,x,t,Fn,omega_e,axis):
+        fni = round(Fn/(1./40)) #0~40 
+        omi = round(omega_e/(1./4)) #0~60
+        if(fni>=41): print(fni)
+        if(omi>=61): print(omi)
+
+        j = round((axis-1)/2)
+
+        coeff =  matrixList[j][fni][omi]*100 #N/cm -> N/m
+        poffset= dmatrixList[j][fni][omi]
+        output = coeff*self.amp*math.sin(self.omega*t + self.k*x + self.phase + poffset)  
+        return output #N
+
+
+class JONSWAP:
+    N = 0    
+    T_1 = 0
+    H_third = 0
+    waves = []
+    delta_omega = 0
+    max_omega = 0
+    def __init__(self, N, T_1, H_third):
+        self.waves = []
+        self.N = N
+        self.T_1 = T_1
+        self.H_third = H_third
+        self.max_omega = 4*math.pi/self.T_1 * 4
+        self.delta_omega = self.max_omega/self.N
+        for i in range(self.N):
+            new_omega = self.delta_omega*(0.5+i)+random.uniform(-self.delta_omega/2,self.delta_omega/2)
+            new_wave = Wave(self.getWaveHeight(new_omega),new_omega,random.uniform(0,2*math.pi))
+            self.waves.append(new_wave)
+
+    def getWaveHeight(self,omega):
+        if(omega!=0):
+            sigma = 0.08
+            Y = math.exp(-((0.191*omega*self.T_1-1)/(math.sqrt(2)*sigma))**2) 
+            S = 155*(self.H_third**2)/(self.T_1**4*omega**5)*math.exp(-944/(self.T_1**4*omega**4))*3.3**Y
+            A = math.sqrt(2*S*self.delta_omega)
+        else:
+            A = 0
+            S = 0
+        return A
+        # return S
+
+    def get(self,x,t):
+        waveheight = 0
+        for w in self.waves:
+            waveheight += w.get(x,t)
+        return waveheight
+
+
+class Water:
+    waves = []
+    def __init__(self, num, random_wave):
+        self.waves = []
+        if(random_wave==True):
+            for i in range(num):
+                newWave = Wave(random.uniform(0.01,0.05),random.uniform(1.2,3),random.uniform(1,10))
+                self.waves.append(newWave)
+        elif(num==0):
+            self.waves.append(Wave(0,0,0))
+        else:
+            self.waves.append(Wave(0.1,10,0))
+            
+    def get(self,x,t):
+        waveheight = 0
+        for w in self.waves:
+            waveheight += w.get(x,t)
+        return waveheight
